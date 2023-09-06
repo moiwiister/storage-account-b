f6877116b9ae0262e70fc5187bda14a800e765d1
Alexandre Debargis <adebargis@microsoft.com>
2023-09-05 20:48:46 -0400

    updated outputs again

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "storage_account_id" {
-  value       = storage-account-module.storage_account.id
+  value       = storage-account-module.storage_account_id
   description = "ID of the storage account."
 }
 
 output "storage_account_name1" {
-  value       = storage-account-module.storage_account.name
+  value       = storage-account-module.storage_account_name1
   description = "Name of the storage account."
 }
